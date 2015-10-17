451186f0efbe53ea0e75d119de1b77ab07c90e7b
Oswaldo Ferreira <oswluizf@gmail.com>
2015-10-17 04:34:32 -0300

    adding basic nicknames support

--- index.js
+++ index.js
@@ -7,22 +7,25 @@ app.get('/', function (req, res) {
 });
 
 io.sockets.on('connection', function (socket) {
+  var userName;
+
   // New connection
   socket.on('user connect', function (user) {
+    userName = user.name;
     io.emit('user connect', user.name + ' has joined.');
   });
 
   // Disconnection
   socket.on('user disconnect', function (user) {
-    io.emit('user disconnect', user.name + ' has disconnected.');
+   io.emit('user disconnect', user.name + ' has disconnected.');
   });
 
   // Chat message
   socket.on('chat message', function (message) {
-    io.emit('chat message', message);
+    io.emit('chat message', userName + ': ' + message);
   });
 });
 
-http.listen(3000, function () {
-  console.log('Listening on: *:3000');
+http.listen(5000, function () {
+  console.log('Listening on: *:5000');
 });
--- tests/index.js
+++ tests/index.js
@@ -1,53 +1,52 @@
 var should = require('should');
 var io = require('socket.io-client'),
-  server = require('../index.js')
+  server = require('../index.js').server
 
-var socketURL = 'http://0.0.0.0:3000';
+var socketURL = 'http://0.0.0.0:5000';
 
-var chatUser1 = {'name':'Tom'};
+var chatUser1 = {'name': 'Tom'};
+var chatUser2 = {'name': 'Jerry'};
 
 var options = {
   transports: ['websocket'],
   'force new connection': true
 };
 
-describe('Chat server', function () {
-  it('broadcast chat message to all clients', function (done) {
-    var client1 = io.connect(socketURL, options);
-    var client2 = io.connect(socketURL, options);
-
-    client1.emit('chat message', 'Hey everyone!');
-    client1.disconnect();
-
-    client2.on('chat message', function (message) {
-      message.should.equal('Hey everyone!');
-      client2.disconnect();
-    });
-
-    done();
-  });
-
-  // A single user
-  it('Should broadcast "user connect" message once they connect', function (done) {
+describe('chat server', function () {
+  it('should broadcast "user connect" channel', function (done) {
     var client = io.connect(socketURL, options);
 
     client.emit('user connect', chatUser1);
 
-    client.on('user connect', function (usersName) {
-      usersName.should.equal(chatUser1.name + " has joined.");
+    client.on('user connect', function (message) {
+      message.should.equal('Tom has joined.');
 
       client.disconnect();
       done();
     });
   });
 
-  it('Should broadcast "user disconnect" once they disconnect', function (done) {
+  it('should broadcast "chat message" channel', function (done) {
+    var client = io.connect(socketURL, options);
+
+    client.emit('user connect', chatUser1);
+    client.emit('chat message', 'Hey everyone!');
+
+    client.on('chat message', function (message) {
+      message.should.equal('Tom: Hey everyone!');
+
+      client.disconnect();
+      done();
+    });
+  });
+
+  it('should broadcast "user disconnect" channel', function (done) {
     var client = io.connect(socketURL, options);
 
     client.emit('user disconnect', chatUser1);
 
-    client.on('user disconnect', function (usersName) {
-      usersName.should.equal(chatUser1.name + " has disconnected.");
+    client.on('user disconnect', function (message) {
+      message.should.equal("Tom has disconnected.");
 
       client.disconnect();
       done();
