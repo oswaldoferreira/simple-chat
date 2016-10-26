1947982b6f6d61b193ddfcb68670744888cc2020
Oswaldo Ferreira <oswluizf@gmail.com>
2016-10-26 15:30:26 -0200

    Introduce start-prod script

---
+++ app/server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Setup logger
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
