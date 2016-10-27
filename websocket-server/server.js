e54855c7db230bf7c5bc18817167ddba3991464e
Oswaldo Ferreira <oswluizf@gmail.com>
2016-10-27 00:51:18 -0200

    Clean websocket server

--- websocket-server/server.js
+++ websocket-server/server.js
@@ -1,15 +1,12 @@
-var path = require('path');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-io.sockets.on('connection', (socket) => {
-  // Forwarding chat message
-  socket.on('chat-message', (message) => {
-    console.log(message);
-    io.emit('chat-message', message);
-  });
+// Forwarding chat message
+socket.on('chat-message', (message) => {
+  console.log(message);
+  io.emit('chat-message', message);
 });
 
 http.listen(process.env.PORT || 8081, () => {
