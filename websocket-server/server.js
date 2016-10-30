17f3c0b230b22b57c6bfd91441c9001b052bcb1b
Oswaldo Ferreira <oswluizf@gmail.com>
2016-10-30 17:35:21 -0200

    Wrap chat-message channel into connection channel

--- websocket-server/server.js
+++ websocket-server/server.js
@@ -4,9 +4,10 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 // Forwarding chat message
-socket.on('chat-message', (message) => {
-  console.log(message);
-  io.emit('chat-message', message);
+io.on('connection', (socket) => {
+  socket.on('chat-message', (message) => {
+    socket.emit('chat-message', message);
+  });
 });
 
 http.listen(process.env.PORT || 8081, () => {
